684cfa4ad9fa4df0de974245d6e2d5c530c74600
Catherine <cat.taylor.taylot@btinternet.com>
2019-02-26 11:17:15 +0000

    Added in main file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+
+int main(){
+
+    return 0;
+}
